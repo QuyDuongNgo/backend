c69c6c60b78fb82ae7330514df7b2c1061b2b98e
QuyDuongNgo <quyduongngo2000@gmail.com>
2024-03-14 10:38:19 +0700

    laravel ui

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/test', function () {
     return '<h1>Hello my project</h1>';
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
